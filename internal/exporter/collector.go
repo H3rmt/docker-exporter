41b3feb8b20b4bf3aef5c2f48531e5f5d9f2b39b
Enrico Stemmer <enrico.stemmer@arca-consult.de>
2026-01-09 19:49:18 +0100

    fix: add hostname to all labels

--- internal/exporter/collector.go
+++ internal/exporter/collector.go
@@ -2,6 +2,8 @@ package exporter
 
 import (
 	"context"
+	"os"
+	"strings"
 
 	"github.com/h3rmt/docker-exporter/internal/docker"
 	"github.com/h3rmt/docker-exporter/internal/log"
@@ -19,151 +21,151 @@ var (
 	exporterInfoDesc = prometheus.NewDesc(
 		"docker_exporter_info",
 		"Information about the docker exporter",
-		[]string{"version"},
+		[]string{"hostname", "version"},
 		nil,
 	)
 	containerInfoDesc = prometheus.NewDesc(
 		"docker_container_info",
 		"Container information",
-		[]string{"container_id", "name", "image_id", "command", "network_mode"},
+		[]string{"hostname", "container_id", "name", "image_id", "command", "network_mode"},
 		nil,
 	)
 	containerNameDesc = prometheus.NewDesc(
 		"docker_container_name",
 		"Name for the container (can be more than one)",
-		[]string{"container_id", "name"},
+		[]string{"hostname", "container_id", "name"},
 		nil,
 	)
 	containerStateDesc = prometheus.NewDesc(
 		"docker_container_state",
 		"Container State (0=created, 1=running, 2=paused, 3=restarting, 4=removing, 5=exited, 6=dead)",
-		[]string{"container_id"},
+		[]string{"hostname", "container_id"},
 		nil,
 	)
 	containerCreatedDesc = prometheus.NewDesc(
 		"docker_container_created_seconds",
 		"Timestamp in seconds when the container was created",
-		[]string{"container_id"},
-		nil,
-	)
-	containerStartedDesc = prometheus.NewDesc(
-		"docker_container_started_seconds",
-		"Timestamp in seconds when the container was started",
-		[]string{"container_id"},
-		nil,
-	)
-	containerFinishedAtDesc = prometheus.NewDesc(
-		"docker_container_finished_at_seconds",
-		"Timestamp in seconds when the container finished",
-		[]string{"container_id"},
+		[]string{"hostname", "container_id"},
 		nil,
 	)
 	containerPortsDesc = prometheus.NewDesc(
 		"docker_container_ports",
 		"Forwarded Ports",
-		[]string{"container_id", "public_port", "private_port", "ip", "type"},
+		[]string{"hostname", "container_id", "public_port", "private_port", "ip", "type"},
+		nil,
+	)
+	containerStartedDesc = prometheus.NewDesc(
+		"docker_container_started_seconds",
+		"Timestamp in seconds when the container was started",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerFinishedAtDesc = prometheus.NewDesc(
+		"docker_container_finished_at_seconds",
+		"Timestamp in seconds when the container finished",
+		[]string{"hostname", "container_id"},
 		nil,
 	)
 	containerSizeRootFsDesc = prometheus.NewDesc(
 		"docker_container_rootfs_size_bytes",
 		"Size of rootfs in this container in bytes",
-		[]string{"container_id"},
+		[]string{"hostname", "container_id"},
 		nil,
 	)
 	containerSizeRwDesc = prometheus.NewDesc(
 		"docker_container_rw_size_bytes",
 		"Size of files that have been created or changed by this container in bytes",
-		[]string{"container_id"},
-		nil,
-	)
-	containerPidsDesc = prometheus.NewDesc(
-		"docker_container_pids",
-		"Number of processes running in the container",
-		[]string{"container_id"},
-		nil,
-	)
-	containerCpuUserMicroSecondsDesc = prometheus.NewDesc(
-		"docker_container_cpu_user_microseconds_total",
-		"Time (in microseconds) spent by tasks in user mode",
-		[]string{"container_id"},
-		nil,
-	)
-	containerCpuKernelMicroSecondsDesc = prometheus.NewDesc(
-		"docker_container_cpu_kernel_microseconds_total",
-		"Time (in microseconds) spent by tasks in kernel mode",
-		[]string{"container_id"},
-		nil,
-	)
-	containerMemLimitKiBDesc = prometheus.NewDesc(
-		"docker_container_mem_limit_kib",
-		"Container memory limit in KiB",
-		[]string{"container_id"},
-		nil,
-	)
-	containerMemUsageKiBDesc = prometheus.NewDesc(
-		"docker_container_mem_usage_kib",
-		"Container memory usage in KiB",
-		[]string{"container_id"},
-		nil,
-	)
-	containerNetSendBytesDesc = prometheus.NewDesc(
-		"docker_container_net_send_bytes_total",
-		"Total number of bytes sent",
-		[]string{"container_id"},
-		nil,
-	)
-	containerNetSendDroppedDesc = prometheus.NewDesc(
-		"docker_container_net_send_dropped_total",
-		"Total number of send packet drop",
-		[]string{"container_id"},
-		nil,
-	)
-	containerNetSendErrorsDesc = prometheus.NewDesc(
-		"docker_container_net_send_errors_total",
-		"Total number of send errors",
-		[]string{"container_id"},
-		nil,
-	)
-	containerNetRecvBytesDesc = prometheus.NewDesc(
-		"docker_container_net_receive_bytes_total",
-		"Total number of bytes received",
-		[]string{"container_id"},
-		nil,
-	)
-	containerNetRecvDroppedDesc = prometheus.NewDesc(
-		"docker_container_net_receive_dropped_total",
-		"Total number of receive packet drop",
-		[]string{"container_id"},
-		nil,
-	)
-	containerNetRecvErrorsDesc = prometheus.NewDesc(
-		"docker_container_net_receive_errors_total",
-		"Total number of receive errors",
-		[]string{"container_id"},
-		nil,
-	)
-	containerBlockInputBytesDesc = prometheus.NewDesc(
-		"docker_container_block_input_total",
-		"Total number of bytes read from disk",
-		[]string{"container_id"},
-		nil,
-	)
-	containerBlockOutputBytesDesc = prometheus.NewDesc(
-		"docker_container_block_output_total",
-		"Total number of bytes written to disk",
-		[]string{"container_id"},
-		nil,
-	)
-	containerExitCodeDesc = prometheus.NewDesc(
-		"docker_container_exit_code",
-		"Exit code of the container",
-		[]string{"container_id"},
+		[]string{"hostname", "container_id"},
 		nil,
 	)
 	containerRestartCountDesc = prometheus.NewDesc(
 		"docker_container_restart_count",
 		"Number of times the container has been restarted",
-		[]string{"container_id"},
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerExitCodeDesc = prometheus.NewDesc(
+		"docker_container_exit_code",
+		"Exit code of the container",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerPidsDesc = prometheus.NewDesc(
+		"docker_container_pids",
+		"Number of processes running in the container",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerCpuUserMicroSecondsDesc = prometheus.NewDesc(
+		"docker_container_cpu_user_microseconds_total",
+		"Time (in microseconds) spent by tasks in user mode",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerCpuKernelMicroSecondsDesc = prometheus.NewDesc(
+		"docker_container_cpu_kernel_microseconds_total",
+		"Time (in microseconds) spent by tasks in kernel mode",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerMemLimitKiBDesc = prometheus.NewDesc(
+		"docker_container_mem_limit_kib",
+		"Container memory limit in KiB",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerMemUsageKiBDesc = prometheus.NewDesc(
+		"docker_container_mem_usage_kib",
+		"Container memory usage in KiB",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerNetSendBytesDesc = prometheus.NewDesc(
+		"docker_container_net_send_bytes_total",
+		"Total number of bytes sent",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerNetSendDroppedDesc = prometheus.NewDesc(
+		"docker_container_net_send_dropped_total",
+		"Total number of send packet drop",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerNetSendErrorsDesc = prometheus.NewDesc(
+		"docker_container_net_send_errors_total",
+		"Total number of send errors",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerNetRecvBytesDesc = prometheus.NewDesc(
+		"docker_container_net_receive_bytes_total",
+		"Total number of bytes received",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerNetRecvDroppedDesc = prometheus.NewDesc(
+		"docker_container_net_receive_dropped_total",
+		"Total number of receive packet drop",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerNetRecvErrorsDesc = prometheus.NewDesc(
+		"docker_container_net_receive_errors_total",
+		"Total number of receive errors",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerBlockInputBytesDesc = prometheus.NewDesc(
+		"docker_container_block_input_total",
+		"Total number of bytes read from disk",
+		[]string{"hostname", "container_id"},
+		nil,
+	)
+	containerBlockOutputBytesDesc = prometheus.NewDesc(
+		"docker_container_block_output_total",
+		"Total number of bytes written to disk",
+		[]string{"hostname", "container_id"},
 		nil,
 	)
 )
@@ -180,11 +182,13 @@ func (c *DockerCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (c *DockerCollector) Collect(ch chan<- prometheus.Metric) {
+	hostname := getHostname()
 	// Export version information
 	ch <- prometheus.MustNewConstMetric(
 		exporterInfoDesc,
 		prometheus.GaugeValue,
 		1,
+		hostname,
 		c.version,
 	)
 
@@ -194,11 +198,11 @@ func (c *DockerCollector) Collect(ch chan<- prometheus.Metric) {
 	if err != nil {
 		log.GetLogger().WarnContext(ctx, "Failed to list running containers", "error", err)
 	} else {
-		formatContainerInfo(ch, containerInfo)
-		formatContainerNames(ch, containerInfo)
-		formatContainerState(ch, containerInfo)
-		formatContainerCreated(ch, containerInfo)
-		formatContainerPorts(ch, containerInfo)
+		formatContainerInfo(ch, hostname, containerInfo)
+		formatContainerNames(ch, hostname, containerInfo)
+		formatContainerState(ch, hostname, containerInfo)
+		formatContainerCreated(ch, hostname, containerInfo)
+		formatContainerPorts(ch, hostname, containerInfo)
 	}
 
 	for _, container := range containerInfo {
@@ -207,12 +211,12 @@ func (c *DockerCollector) Collect(ch chan<- prometheus.Metric) {
 		if err != nil {
 			log.GetLogger().WarnContext(ctx, "Failed to inspect container", "error", err, "container_id", id)
 		} else {
-			formatContainerStarted(ch, id, inspect)
-			formatContainerExitCode(ch, id, inspect)
-			formatContainerRestartCount(ch, id, inspect)
-			formatContainerFinished(ch, id, inspect)
-			formatContainerSizeRootFs(ch, id, inspect)
-			formatContainerSizeRw(ch, id, inspect)
+			formatContainerStarted(ch, hostname, id, inspect)
+			formatContainerExitCode(ch, hostname, id, inspect)
+			formatContainerRestartCount(ch, hostname, id, inspect)
+			formatContainerFinished(ch, hostname, id, inspect)
+			formatContainerSizeRootFs(ch, hostname, id, inspect)
+			formatContainerSizeRw(ch, hostname, id, inspect)
 		}
 	}
 
@@ -222,19 +226,19 @@ func (c *DockerCollector) Collect(ch chan<- prometheus.Metric) {
 		if err != nil {
 			log.GetLogger().WarnContext(ctx, "Failed to get container stats", "error", err, "container_id", id)
 		} else {
-			formatContainerPids(ch, id, stat)
-			formatContainerCpuUserMicroSeconds(ch, id, stat)
-			formatContainerCpuKernelMicroSeconds(ch, id, stat)
-			formatContainerMemLimitKiB(ch, id, stat)
-			formatContainerMemUsageKiB(ch, id, stat)
-			formatContainerNetSendBytes(ch, id, stat)
-			formatContainerNetSendDropped(ch, id, stat)
-			formatContainerNetSendErrors(ch, id, stat)
-			formatContainerNetRecvBytes(ch, id, stat)
-			formatContainerNetRecvDropped(ch, id, stat)
-			formatContainerNetRecvErrors(ch, id, stat)
-			formatBlockOutputBytes(ch, id, stat)
-			formatBlockInputBytes(ch, id, stat)
+			formatContainerPids(ch, hostname, id, stat)
+			formatContainerCpuUserMicroSeconds(ch, hostname, id, stat)
+			formatContainerCpuKernelMicroSeconds(ch, hostname, id, stat)
+			formatContainerMemLimitKiB(ch, hostname, id, stat)
+			formatContainerMemUsageKiB(ch, hostname, id, stat)
+			formatContainerNetSendBytes(ch, hostname, id, stat)
+			formatContainerNetSendDropped(ch, hostname, id, stat)
+			formatContainerNetSendErrors(ch, hostname, id, stat)
+			formatContainerNetRecvBytes(ch, hostname, id, stat)
+			formatContainerNetRecvDropped(ch, hostname, id, stat)
+			formatContainerNetRecvErrors(ch, hostname, id, stat)
+			formatBlockOutputBytes(ch, hostname, id, stat)
+			formatBlockInputBytes(ch, hostname, id, stat)
 		}
 	}
 }
@@ -246,4 +250,9 @@ func RegisterCollectorsWithRegistry(cli *docker.Client, reg *prometheus.Registry
 	} else {
 		reg.MustRegister(collector)
 	}
+}
+
+func getHostname() string {
+	hn, _ := os.ReadFile("/etc/hostname")
+	return strings.TrimSpace(string(hn))
 }
--- internal/exporter/formatContainerStat.go
+++ internal/exporter/formatContainerStat.go
@@ -5,118 +5,132 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func formatContainerPids(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerPids(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerPidsDesc,
 		prometheus.GaugeValue,
 		float64(stat.PIds),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerCpuUserMicroSeconds(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerCpuUserMicroSeconds(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerCpuUserMicroSecondsDesc,
 		prometheus.CounterValue,
 		float64(stat.CPUinUserModeMicroSec),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerCpuKernelMicroSeconds(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerCpuKernelMicroSeconds(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerCpuKernelMicroSecondsDesc,
 		prometheus.CounterValue,
 		float64(stat.CPUinKernelModeMicroSec),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerMemLimitKiB(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerMemLimitKiB(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerMemLimitKiBDesc,
 		prometheus.GaugeValue,
 		float64(stat.MemoryLimitKiB),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerMemUsageKiB(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerMemUsageKiB(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerMemUsageKiBDesc,
 		prometheus.GaugeValue,
 		float64(stat.MemoryUsageKiB),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerNetSendBytes(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerNetSendBytes(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerNetSendBytesDesc,
 		prometheus.CounterValue,
 		float64(stat.NetSendBytes),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerNetSendDropped(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerNetSendDropped(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerNetSendDroppedDesc,
 		prometheus.CounterValue,
 		float64(stat.NetSendDropped),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerNetSendErrors(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerNetSendErrors(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerNetSendErrorsDesc,
 		prometheus.CounterValue,
 		float64(stat.NetSendErrors),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerNetRecvBytes(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerNetRecvBytes(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerNetRecvBytesDesc,
 		prometheus.CounterValue,
 		float64(stat.NetRecvBytes),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerNetRecvDropped(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerNetRecvDropped(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerNetRecvDroppedDesc,
 		prometheus.CounterValue,
 		float64(stat.NetRecvDropped),
+		hostname,
 		containerID,
 	)
 }
 
-func formatContainerNetRecvErrors(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatContainerNetRecvErrors(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerNetRecvErrorsDesc,
 		prometheus.CounterValue,
 		float64(stat.NetRecvErrors),
+		hostname,
 		containerID,
 	)
 }
 
-func formatBlockOutputBytes(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
-	ch <- prometheus.MustNewConstMetric(
-		containerBlockOutputBytesDesc,
-		prometheus.CounterValue,
-		float64(stat.BlockOutputBytes),
-		containerID,
-	)
-}
-func formatBlockInputBytes(ch chan<- prometheus.Metric, containerID string, stat docker.ContainerStats) {
+func formatBlockInputBytes(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
 	ch <- prometheus.MustNewConstMetric(
 		containerBlockInputBytesDesc,
 		prometheus.CounterValue,
 		float64(stat.BlockInputBytes),
+		hostname,
+		containerID,
+	)
+}
+
+func formatBlockOutputBytes(ch chan<- prometheus.Metric, hostname string, containerID string, stat docker.ContainerStats) {
+	ch <- prometheus.MustNewConstMetric(
+		containerBlockOutputBytesDesc,
+		prometheus.CounterValue,
+		float64(stat.BlockOutputBytes),
+		hostname,
 		containerID,
 	)
 }
