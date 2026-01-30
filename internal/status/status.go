2328d00583d0f04ec823da725366ea6c684fa5b4
Enrico Stemmer <enrico.stemmer@arca-consult.de>
2026-01-30 20:06:31 +0100

    feat: enhance web UI, update status API, and refine documentation

--- internal/status/status.go
+++ internal/status/status.go
@@ -9,32 +9,35 @@ import (
 )
 
 type Response struct {
-	Status  string `json:"status"`
-	Error   string `json:"error"`
-	Version string `json:"version"`
+	Status        string `json:"status"`
+	DockerError   string `json:"docker_error,omitempty"`
+	DockerVersion string `json:"docker_version,omitempty"`
+	Version       string `json:"version"`
 }
 
-func HandleStatus(cli *docker.Client) http.Handler {
+func HandleStatus(cli *docker.Client, version string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		w.Header().Set("Content-Type", "application/json")
 
 		response := Response{
-			Status:  "healthy",
-			Error:   "",
-			Version: "",
+			Status:        "healthy",
+			DockerError:   "",
+			DockerVersion: "",
+			Version:       version,
 		}
 
 		// Check if Docker daemon is responding
 		ver, err := cli.Ping(ctx)
+		log.GetLogger().Log(ctx, log.LevelTrace, "Checking Docker daemon health", "version", ver, "error", err)
 		if err != nil {
 			log.GetLogger().ErrorContext(ctx, "Docker daemon is not responding", "error", err)
 			response.Status = "unhealthy"
-			response.Error = err.Error()
+			response.DockerError = err.Error()
 			w.WriteHeader(http.StatusServiceUnavailable)
 		} else {
+			response.DockerVersion = ver
 			w.WriteHeader(http.StatusOK)
-			response.Version = ver
 		}
 
 		err = json.NewEncoder(w).Encode(response)
